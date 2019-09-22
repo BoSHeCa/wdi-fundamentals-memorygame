cf195c70d16379156ded52871c76a11292a80ead
“bosheca” <bosheca@protonmail.com>
2019-09-22 20:32:51 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,3 @@
-console.log("Up and running!");
-
 var cards = [
     {
         rank: 'queen',
@@ -22,7 +20,48 @@ var cards = [
         cardImage: 'images/king-of-diamonds.png'
     },
  ];
-
-function refreshPage(){
+ //Creating an empty array for selected cards
+ var cardsInPlay = [];
+ 
+ 
+ //Function to check if user got a match
+ var checkForMatch = function () {
+     if (cardsInPlay[0] === cardsInPlay[1]) {
+         alert("You found a match!");
+     } 
+     else {
+         alert("Sorry, try again.");
+     }
+ };
+ 
+ //Function to place the selected cards to to array of playing cards
+ var flipCard = function() {
+     cardId = this.getAttribute('data-id');
+     console.log(cardId);
+     cardsInPlay.push(cards[cardId].rank);
+     this.setAttribute('src', cards[cardId].cardImage);
+ 
+     if (cardsInPlay.length === 2){
+         checkForMatch();
+         cardsInPlay = [];
+     }
+ 
+ 
+ };
+ //Function to create a board and opening them up on user click
+ var createBoard = function () {
+   for (var i = 0; i < cards.length; i++) {
+     var cardElement = document.createElement('img');
+     cardElement.setAttribute('src', 'images/back.png');
+     cardElement.setAttribute('data-id', i);
+     cardElement.addEventListener('click', flipCard);
+     document.getElementById('game-board').appendChild(cardElement);
+ 
+   }
+ 
+ }
+ createBoard();
+ 
+ function refreshPage(){
     window.location.reload();
-} 
+}  
