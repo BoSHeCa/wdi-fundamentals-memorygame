fcec9d0865fc5ecf1c6626ca8e60b641ade32b7b
“bosheca” <bosheca@protonmail.com>
2019-09-22 20:27:31 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,3 +1,27 @@
+console.log("Up and running!");
+
+var cards = [
+    {
+        rank: 'queen',
+        suit: 'hearts',
+        cardImage: 'images/queen-of-hearts.png'
+    },
+    {
+        rank: 'queen',
+        suit: 'diamonds',
+        cardImage: 'images/queen-of-diamonds.png'
+    },
+       {
+        rank: 'king',
+        suit: 'hearts',
+        cardImage: 'images/king-of-hearts.png'
+    },
+       {
+        rank: 'king',
+        suit: 'diamonds',
+        cardImage: 'images/king-of-diamonds.png'
+    },
+ ];
 
 function refreshPage(){
     window.location.reload();
